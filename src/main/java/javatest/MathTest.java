44eb51e211dcdf6c9fa2c2d8c6cbedd0ad7f4952
liujixiang <1030375081@qq.com>
2022-09-21 08:56:58 +0800

    提交

---
+++ src/main/java/javatest/MathTest.java
@@ -0,0 +1,20 @@
+package javatest;
+
+
+/**
+ * @author ljx
+ * @version 1.0.0
+ * @create 2022/8/22 17:12
+ */
+
+public class MathTest {
+
+	public static void main(String[] args) {
+		System.out.println(Math.floor(1.5));
+		System.out.println(Math.floor(-1.5));
+		System.out.println(Math.round(1.5));
+		System.out.println(Math.round(-1.5));
+	}
+
+
+}
