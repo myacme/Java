23aa59984fca9d5227e7d513965958690f01da5b
liujixiang <1030375081@qq.com>
2023-02-15 11:24:11 +0800

    提交

--- boot/src/main/java/com/BootApplication.java
+++ boot/src/main/java/com/BootApplication.java
@@ -1,5 +1,6 @@
 package com;
 
+import com.example.boot.util.SpringContextUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ public class BootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootApplication.class, args);
+		SpringContextUtil.getBean("loginUser");
 	}
 
 }
