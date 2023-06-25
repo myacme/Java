7332b1acc43d45d7b2058a455fa449371a909232
liujixiang <1030375081@qq.com>
2023-06-25 14:55:36 +0800

    提交

---
+++ boot/src/main/java/com/example/boot/Interceptor/InterceptResponse.java
@@ -0,0 +1,30 @@
+package com.example.boot.Interceptor;
+
+
+import com.example.boot.Base64Converter;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.lang.Nullable;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+
+/**
+ * @author liujixiang
+ */
+@ControllerAdvice
+public class InterceptResponse implements ResponseBodyAdvice<Object> {
+	@Override
+	public boolean supports(MethodParameter methodParameter, Class<? extends HttpMessageConverter<?>> aClass) {
+		return true;
+	}
+
+	@Nullable
+	@Override
+	public Object beforeBodyWrite(@Nullable Object body, MethodParameter methodParameter, MediaType mediaType, Class<? extends HttpMessageConverter<?>> aClass, ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse) {
+		return Base64Converter.encode(body);
+	}
+}
