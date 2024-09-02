d419888f03193942279b72978fc1359e373cc77a
liujx <1030375081@qq.com>
2024-09-02 17:06:28 +0800

    Default Changelist

--- src/main/java/javatest/StringTest.java
+++ src/main/java/javatest/StringTest.java
@@ -1,7 +1,5 @@
 package javatest;
 
-import java.util.*;
-
 /**
  * 〈一句话功能简述〉<br>
  * 〈〉
@@ -11,60 +9,28 @@ import java.util.*;
  * @since 1.0.0
  */
 
- class StringTest {
-    public static void main(String[] args) {
-        String s = " ";
-        StringBuffer stringBuffer = new StringBuffer();
-        while (true) {
-
-            try {
-                Scanner reader = new Scanner(System.in);
-                s = reader.nextLine();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            if (s.equals("#")) {
-                System.out.println("输入结束！");
-                break;
-            }
-            stringBuffer.append(s).append(";");
-        }
-        String str = stringBuffer.substring(0);
-        String[] slist = str.split(";");
-        Set<String> stringSet = new HashSet<>();
-        List<Integer > list = new ArrayList<>();
-        for (int i = 0; i < slist.length; i++) {
-            String[] slist1 = slist[i].split(" ");
-            for (int j = 0; j < slist1.length; j++) {
-                stringSet.add(slist1[j]);
-            }
-            list.add( stringSet.size());
-            stringSet.clear();
-        }
-        for (Integer e : list) {
-            System.out.println(e);
-        }
-
-
+class StringTest {
+    public static void test() {
+        String s = "java";
+        String t = "wolai";
+        System.out.println(s.intern());
+        System.out.println(t.intern());
+        System.out.println(s.intern() == s);
+        System.out.println(t.intern() == t);
+        String s1 = new StringBuffer("java").toString();
+        System.out.println(s1.intern() == s1);
+        System.out.println(s == s1);
+        String t1 = new StringBuffer("wolail").toString();
+        System.out.println(t1.intern() == t1);
     }
-	static class Intern{
-		public static void main(String[] args) {
-			String s = "java";
-			String t = "wolai";
- 			System.out.println(s.intern());
-			System.out.println(t.intern());
-			System.out.println(s.intern() == s);
-			System.out.println(t.intern() == t);
 
-			String s1 = new StringBuffer("java").toString();
-			System.out.println(s1.intern() == s1);
-			System.out.println(s == s1);
+    public static void test2(String str) {
+        str = "hello world";
+    }
 
-			String t1 = new StringBuffer("wolail").toString();
-			System.out.println(t1.intern() == t1);
-
-		}
-	}
-
-
+    public static void main(String[] args) {
+        String str = "hello";
+        test2(str);
+        System.out.println(str);
+    }
 }
