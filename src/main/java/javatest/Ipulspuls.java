debf5ce0a4d84faeb2bda184d399057c57486ef1
liujx <1030375081@qq.com>
2024-09-05 11:32:42 +0800

    Default Changelist

---
+++ src/main/java/javatest/Ipulspuls.java
@@ -0,0 +1,17 @@
+package javatest;
+
+/**
+ * @author ljx
+ * @version 1.0.0
+ * @create 2024/9/5 上午11:03
+ */
+public class Ipulspuls {
+
+    public static void main(String[] args) {
+        int i = 1;
+        int j=i++;
+        System.out.println(j);
+        System.out.println(i);
+        i++;
+    }
+}
