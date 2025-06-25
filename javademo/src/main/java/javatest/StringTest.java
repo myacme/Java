e9edc55d7dd3af5aa47108f8999175ec88022374
liujx <1030375081@qq.com>
2025-06-25 15:41:38 +0800

    refactor(StringTest): 修正字符串初始化并添加字符串常量池测试方法
    - 修正了 String t1 的初始化，将 "wolail" 改为 "wolai"
    - 添加了多个测试方法（test3 到 test6）来验证字符串常量池的行为- 在 test3 方法中添加了对字符串拼接和 intern() 方法的详细解释
    - 修改了 main 方法的调用，以测试新添加的 test6 方法

--- javademo/src/main/java/javatest/StringTest.java
+++ javademo/src/main/java/javatest/StringTest.java
@@ -20,7 +20,7 @@ class StringTest {
         String s1 = new String("java");
         System.out.println(s1.intern() == s1);
         System.out.println(s == s1);
-        String t1 = new String("wolail");
+        String t1 = new String("wolai");
         System.out.println(t1.intern() == t1);
     }
 
@@ -29,9 +29,63 @@ class StringTest {
         System.out.println(str);
     }
 
+    /**
+     * new string("ab")
+     * 对象1：New String()
+     * 对象2: 常量池“ab”
+     *
+     *
+     * new string("a")+ new string("b")呢?
+     * 对象1: + 隐式创建的 new stringBuilder()
+     * 对象2: new string()
+     * 对象3: 常量池中的"a"
+     * 对象4: new string()
+     * 对象5: 常量池中的"b"
+     * 深入剖析:stringBuilder的tostring():
+     * 对象6: new string(“ab")
+     * 强调-下，tostring()的调用，在字符串常量池中，有生成"ab"
+     */
+    public static void test3() {
+        String s1 = new String("1");
+        s1.intern();
+        String s2 = "1";
+        System.out.println(s1 == s2);
+        String s3 = new String("1") + new String("1");
+        s3.intern();
+        //jdk6:创建了一个新的对象"11”,也就有新的地址。
+        //jdk7:此时常量中并没有创建”11”,而是创建一个指间堆空间中new string("11”)的地址
+        String s4 = "11";
+        System.out.println(s3 == s4);
+    }
+
+    public static void test4() {
+        String s1 = new String("1") + new String("1");
+        String s2 = s1.intern();
+        //jdk6:创建了一个新的对象"11”,也就有新的地址。
+        //jdk7:此时常量中并没有创建”11”,而是创建一个指间堆空间中new string("11”)的地址
+        System.out.println(s1 == "11");
+        System.out.println(s2 == "11");
+    }
+    public static void test5() {
+        String s = "11";
+        String s1 = new String("1") + new String("1");
+        String s2 = s1.intern();
+        //jdk6:创建了一个新的对象"11”,也就有新的地址。
+        //jdk7:此时常量中并没有创建”11”,而是创建一个指间堆空间中new string("11”)的地址
+        System.out.println(s1 == "11");
+        System.out.println(s2 == "11");
+    }
+    public static void test6() {
+        String s = new String("11");
+        String s1 = s.intern();
+        //jdk6:创建了一个新的对象"11”,也就有新的地址。
+        //jdk7:此时常量中并没有创建”11”,而是创建一个指间堆空间中new string("11”)的地址
+        String s2 = "11";
+        System.out.println(s == s2);
+    }
+
+
     public static void main(String[] args) {
-        String str = "hello";
-        test2(str);
-        System.out.println(str);
+        test6();
     }
 }
