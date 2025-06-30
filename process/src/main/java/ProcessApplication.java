35ec73039b80aafe67c2bb4c27a4583755f7d226
liujx <1030375081@qq.com>
2025-06-30 16:21:17 +0800

    refactor(StringTest): 修正字符串初始化并添加字符串常量池测试方法
    - 修正了 String t1 的初始化，将 "wolail" 改为 "wolai"
    - 添加了多个测试方法（test3 到 test6）来验证字符串常量池的行为- 在 test3 方法中添加了对字符串拼接和 intern() 方法的详细解释
    - 修改了 main 方法的调用，以测试新添加的 test6 方法

--- process/src/main/java/ProcessApplication.java
+++ process/src/main/java/ProcessApplication.java
@@ -2,6 +2,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+/**
+ * @author MyAcme
+ */
 @SpringBootApplication
 @EnableTransactionManagement
 public class ProcessApplication {
