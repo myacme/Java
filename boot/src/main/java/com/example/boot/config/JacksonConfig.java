1b8f1095b75cd653bf4fa17321bd89091d44d2a8
liujx <1030375081@qq.com>
2025-08-06 17:15:58 +0800

    feat(Jackson): 添加时间格式化配置
    - 新增 Jackson2ObjectMapperBuilder 配置，用于格式化日期和 LocalDatetime
    - 设置日期格式为 "yyyy-MM-dd HH:mm:ss"
    - 设置时区为 GMT+8
    - 添加 LocalDateTime 的序列化和反序列化配置

--- boot/src/main/java/com/example/boot/config/JacksonConfig.java
+++ boot/src/main/java/com/example/boot/config/JacksonConfig.java
@@ -2,21 +2,57 @@ package com.example.boot.config;
 
 
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.TimeZone;
 
 /**
- * json序列化配置  Long -> String  解决雪花算法id精度丢失问题 number精度丢失
+ * json序列化配置
+ *
  * @author ljx
  * @version 1.0.0
  * @create 2025/8/1 下午1:20
  */
 @Configuration
 public class JacksonConfig {
+
+    /**
+     * json序列化配置  Long -> String  解决雪花算法id精度丢失问题 number精度丢失
+     */
     @Bean
     public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
         return builder -> builder.serializerByType(Long.class, ToStringSerializer.instance)
                 .serializerByType(Long.TYPE, ToStringSerializer.instance);
+    }
+
+    /**
+     * json序列化  格式化时间
+     *
+     *
+     * yml：
+     * spring:
+     *   jackson:
+     *     date-format: yyyy-MM-dd HH:mm:ss
+     *     time-zone: GMT+8
+     *
+     * @return
+     */
+    @Bean
+    public Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder() {
+        return new Jackson2ObjectMapperBuilder()
+                //格式化date
+                .dateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"))
+                .timeZone(TimeZone.getTimeZone("GMT+8"))
+                //格式化LocalDateTime java8
+                .serializerByType(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
+                .deserializerByType(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
     }
 }
