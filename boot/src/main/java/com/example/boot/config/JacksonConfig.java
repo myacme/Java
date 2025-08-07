d22a562ceca6b90e270d2002306890cf5eaccd37
liujx <1030375081@qq.com>
2025-08-07 14:31:01 +0800

    refactor(jackson): 优化 JSON 序列化配置
    -移除了 Long 类型到 String 的序列化配置方法
    - 合并了 Jackson2ObjectMapperBuilder 的配置
    - 优化了代码结构，提高了可读性

--- boot/src/main/java/com/example/boot/config/JacksonConfig.java
+++ boot/src/main/java/com/example/boot/config/JacksonConfig.java
@@ -7,7 +7,6 @@ import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
@@ -26,33 +25,29 @@ public class JacksonConfig {
 
     /**
      * json序列化配置  Long -> String  解决雪花算法id精度丢失问题 number精度丢失
-     */
-    @Bean
-    public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
-        return builder -> builder.serializerByType(Long.class, ToStringSerializer.instance)
-                .serializerByType(Long.TYPE, ToStringSerializer.instance);
-    }
-
-    /**
+     * <p>
      * json序列化  格式化时间
-     *
-     *
+     * <p>
+     * <p>
      * yml：
      * spring:
-     *   jackson:
-     *     date-format: yyyy-MM-dd HH:mm:ss
-     *     time-zone: GMT+8
+     * jackson:
+     * date-format: yyyy-MM-dd HH:mm:ss
+     * time-zone: GMT+8
      *
      * @return
      */
     @Bean
-    public Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder() {
-        return new Jackson2ObjectMapperBuilder()
-                //格式化date
-                .dateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"))
-                .timeZone(TimeZone.getTimeZone("GMT+8"))
-                //格式化LocalDateTime java8
-                .serializerByType(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
-                .deserializerByType(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+    public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
+        return builder ->
+                //Long -> String
+                builder.serializerByType(Long.class, ToStringSerializer.instance)
+                        .serializerByType(Long.TYPE, ToStringSerializer.instance)
+                        //格式化date
+                        .dateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"))
+                        .timeZone(TimeZone.getTimeZone("GMT+8"))
+                        //格式化LocalDateTime java8
+                        .serializerByType(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
+                        .deserializerByType(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
     }
 }
